ea11f392d342f8e89be28a1540183e780bddbfdc
JoaoVSouto <joaovitorss99@hotmail.com>
2019-03-05 12:24:17 -0300

    Adding Mine Component

--- mines/App.js
+++ mines/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import params from './src/params';
+import Field from './src/components/Field';
 
 export default class App extends React.Component {
   render() {
@@ -9,6 +10,16 @@ export default class App extends React.Component {
         <Text style={styles.welcome}>Iniciando o Mines!</Text>
         <Text style={styles.instructions}>Tamanho da grade:
             {params.getRowsAmount()}x{params.getColumnsAmount()}</Text>
+        
+        <Field />
+        <Field opened />
+        <Field opened nearMines={1} />
+        <Field opened nearMines={2} />
+        <Field opened nearMines={3} />
+        <Field opened nearMines={6} />
+        <Field mined />
+        <Field opened mined />
+        <Field opened mined exploded />
       </View>
     );
   }
